5b3f4cdacf56473c0c75d3f7ef3056602828232c
DEEPESH SAXENA <deepeshsaxena18@gmail.com>
2025-03-20 23:30:38 +0530

    Added airports seeders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/ServerConfig');
 const ApiRoutes = require('./routes/index'); 
 
+const db = require('./models/index');
+// const { City , Airport } = require('./models/index');
 
 const setupAndStartServer = async () => {
     // create new express oject
@@ -14,8 +16,42 @@ const setupAndStartServer = async () => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/api', ApiRoutes);
 
-  app.listen(3000, () => {
+  app.listen(3000, async () => {
     console.log(`Server is running on port ${PORT}`);
+    if(process.env.SYNC_DB){
+      db.sequelize.sync({ alter: true });
+    }
+    //different ways to query the database
+    // const airports = await Airport.findAll({
+    //   include: [
+    //     {
+    //       model: City
+    //     }
+    //   ]
+    // });
+    // console.log(airports);
+
+    // db.sequelize.sync({ alter: true });
+
+    // Different ways to query the database
+    // const city = await City.findOne({
+    //   where: {
+    //     id: 7
+    //   }
+    // });
+    // const airports = await city.getAirports();
+
+    // // another way to query the database
+    // const newAirport = await Airport.findOne({
+    //   where: {
+    //     id: 8
+    //   }
+    // });
+    // await city.addAirport(newAirport);
+
+
+    // console.log(city , airports);
+
   });
 }
 
